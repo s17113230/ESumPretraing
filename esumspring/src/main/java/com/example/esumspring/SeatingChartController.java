ac112b5b954d26ab8a496c3688d304c8fb414af9
CrazyMM <s116764502@gmail.com>
2023-08-01 21:35:08 +0800

    新增cross需求

--- esumspring/src/main/java/com/example/esumspring/SeatingChartController.java
+++ esumspring/src/main/java/com/example/esumspring/SeatingChartController.java
@@ -3,6 +3,7 @@ package com.example.esumspring;
 import com.example.esumspring.model.SeatingChart;
 import com.example.esumspring.service.SeatingChartService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value="/api/1.0")
+@CrossOrigin("http://localhost:8080")
 public class SeatingChartController {
 
     @Autowired
