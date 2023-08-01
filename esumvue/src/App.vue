5be5982ed0ef2bc9a3c720dcf59cc0bbd9715a9a
CrazyMM <s116764502@gmail.com>
2023-08-01 21:33:13 +0800

    1. 修正vue前端環境問題

--- esumvue/src/App.vue
+++ esumvue/src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <SeatingChart />
   </div>
 </template>
 
 <script>
+import SeatingChart from './components/SeatingChart.vue';
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    SeatingChart
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
