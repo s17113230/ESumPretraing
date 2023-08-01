56f4f3658c419a3db9931d7b5550c73a3b2503ba
CrazyMM <s116764502@gmail.com>
2023-08-02 01:52:06 +0800

    Revert "新增Xss防範機制"
    This reverts commit 1e3732963550a55c1f651e8cbbd6d5c666ffcd48.

--- esumspring/src/main/java/com/example/esumspring/EmployeeController.java
+++ esumspring/src/main/java/com/example/esumspring/EmployeeController.java
@@ -1,6 +1,5 @@
 package com.example.esumspring;
 
-import com.example.esumspring.common.XssFilters;
 import com.example.esumspring.model.Employee;
 import com.example.esumspring.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,11 +26,6 @@ public class EmployeeController {
 
     @PatchMapping("/users")
     public List<Employee> updateSeatSeqByEmpId(@RequestParam("empId") Integer empId,@RequestParam("seatSeq") Integer seatSeq){
-        if (XssFilters.stripXSS(String.valueOf(empId)).equals("具有非法關鍵字") || XssFilters.stripXSS(String.valueOf(seatSeq)).equals("具有非法關鍵字")){
-            return null;
-        }
-        empId = Integer.parseInt(XssFilters.stripXSS(String.valueOf(empId)));
-        seatSeq = Integer.parseInt(XssFilters.stripXSS(String.valueOf(seatSeq)));
         return employeeService.updateSeatSeqByEmpId(empId, seatSeq);
     }
 }
